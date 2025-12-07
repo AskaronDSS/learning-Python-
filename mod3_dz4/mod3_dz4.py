0024518d3dbac37c6d20a547df2f3bd04ced96ed
AskaronDSS <dssergey94@gmail.com>
2025-12-07 22:25:43 +0200

    last version

--- mod3_dz4/mod3_dz4.py
+++ mod3_dz4/mod3_dz4.py
@@ -11,15 +11,15 @@ def test_add_numbers():
     assert add_numbers(5,0) == 5
 
 # Вторая часть задания. Подключиться к АПИ и сделать тест на подключение.
-# def connect_API(city = 'Kiev', key = '668498292f008dbe9785969c99c8dd16'):
-#     response = requests.get(
-#                 f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}'
-#         )
-#     if response.status_code == 200:
-#         print('Connect OK')
-#     else:
-#         raise Exception
-#
-#
-# def test_conn():
-#     assert connect_API() == 200
+def connect_API(city = 'Kiev', key = '668498292f008dbe9785969c99c8dd16'):
+    response = requests.get(
+                f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}'
+        )
+    if response.status_code == 200:
+        print('Connect OK')
+        return response.status_code
+    else:
+        raise Exception
+
+def test_conn():
+    assert connect_API() == 200
